ca8572ec2862c14f1a1e442397346967cbafb97c
Takuma <takuma012011@gmail.com>
2020-08-29 17:12:43 +0900

    Add Template

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'clean_blogs#index'
+  get  'about' 	=> 'clean_blogs#about'
+  get  'post' 	=> 'clean_blogs#post'
+  get  'contact' 	=> 'clean_blogs#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
